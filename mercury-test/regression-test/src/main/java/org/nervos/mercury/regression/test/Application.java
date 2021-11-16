1185623cf91cc6e0012f00a299fa5331d6e393e5
“zhengjianhui” <815168733@qq.com>
2021-11-16 15:52:25 +0800

    feat(mercury-test): Removing web dependencies

--- mercury-test/regression-test/src/main/java/org/nervos/mercury/regression/test/Application.java
+++ mercury-test/regression-test/src/main/java/org/nervos/mercury/regression/test/Application.java
@@ -1,13 +1,17 @@
 package org.nervos.mercury.regression.test;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.nervos.mercury.regression.test.service.FetchDataService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @MapperScan(basePackages = "org.nervos.mercury.fetch.data.mapper")
 public class Application {
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+    FetchDataService fetchDataService = context.getBean(FetchDataService.class);
+    fetchDataService.fetchData();
   }
 }
