03fe6b57b1fb9e245c30efbefd6c11fce0331c6c
“zhengjianhui” <815168733@qq.com>
2021-11-25 17:37:14 +0800

    feat(mercury-benchmark): Update benchmark script

--- mercury-benchmark/script/mainnet/build_smart_transfer_transaction.lua
+++ mercury-benchmark/script/mainnet/build_smart_transfer_transaction.lua
@@ -1,5 +1,5 @@
 wrk.method = "POST"
-wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_simple_transfer_transaction\",\"params\":[{\"asset_info\":{\"asset_type\":\"UDT\",\"udt_hash\":\"0x613c88b598885bdec6dcc3d3687db5ae6be9a64e96e516c08e0bebe233d23304\"},\"from\":[\"ckb1qyq8jy6e6hu89lzwwgv9qdx6p0kttl4uax9srq9shl\"],\"to\":[{\"address\":\"ckt1qyq27z6pccncqlaamnh8ttapwn260egnt67ss2cwvz\",\"amount\":\"20\"}],\"fee_rate\":1000}],\"id\":100}"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_simple_transfer_transaction\",\"params\":[{\"asset_info\":{\"asset_type\":\"UDT\",\"udt_hash\":\"0xf21e7350fa9518ed3cbb008e0e8c941d7e01a12181931d5608aa366ee22228bd\"},\"from\":[\"ckb1qyq8jy6e6hu89lzwwgv9qdx6p0kttl4uax9srq9shl\"],\"to\":[{\"address\":\"ckb1qyq8jy6e6hu89lzwwgv9qdx6p0kttl4uax9srq9shl\",\"amount\":\"20\"}],\"fee_rate\":1000}],\"id\":100}"
 wrk.headers["Content-Type"] = "application/json"
 
 function response(status, headers, body)
