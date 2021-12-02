02913988583d3d7480ab7507bed183bb631bc057
fjchen7 <fjchen7@gmail.com>
2021-12-02 13:30:43 +0800

    feat: add `get_tip` test cases `testGetTip`

--- mercury-test/regression-test/src/test/java/api/indexer/TipTest.java
+++ mercury-test/regression-test/src/test/java/api/indexer/TipTest.java
@@ -1,5 +1,7 @@
 package api.indexer;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.nervos.indexer.model.resp.TipResponse;
@@ -7,15 +9,27 @@ import org.nervos.indexer.model.resp.TipResponse;
 import java.io.IOException;
 
 import constant.ApiFactory;
+import org.nervos.mercury.regression.test.RpcMethods;
+import org.nervos.mercury.regression.test.domian.GsonFactory;
+import org.nervos.mercury.regression.test.domian.cases.CaseWriter;
+import org.nervos.mercury.regression.test.domian.rpc.RpcService;
 
-@Disabled
 public class TipTest {
+  String method = RpcMethods.INDEXER_GET_TIP;
+  RpcService rpc = new RpcService("http://127.0.0.1:8116");
+  CaseWriter cw = new CaseWriter(method, "./src/main/resources");
+
+  /**
+   * Params Test
+   * - Param: null
+   * - Value: null
+   * - Type: Positive Testing
+   */
   @Test
-  void getTip() {
+  void testGetTip() {
     try {
-      TipResponse tip = ApiFactory.getApi().getTip();
-      System.out.println(tip.blockHash);
-      System.out.println(tip.blockNumber);
+      JsonElement response = rpc.post(method, null);
+      cw.write("testGetTip", null, response, null);
     } catch (IOException e) {
       e.printStackTrace();
     }
