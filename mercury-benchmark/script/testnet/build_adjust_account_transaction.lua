f63dd0909da25f13e61867464a5ba280ddab2646
“zhengjianhui” <815168733@qq.com>
2021-12-14 11:45:50 +0800

    feat(mercury-benchmark): Update script

--- mercury-benchmark/script/testnet/build_adjust_account_transaction.lua
+++ mercury-benchmark/script/testnet/build_adjust_account_transaction.lua
@@ -1,5 +1,5 @@
 wrk.method = "POST"
-wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_adjust_account_transaction\",\"params\":[{\"item\":{\"type\":\"Identity\",\"value\":\"0x001d71b9131063f7cbc8077f0acf22b7e83290d1b1\"},\"from\":[{\"type\":\"Identity\",\"value\":\"0x00202647fecc5b9d8cbdb4ae7167e40f5ab1e4baaf\"}],\"asset_info\":{\"asset_type\":\"UDT\",\"udt_hash\":\"0xf21e7350fa9518ed3cbb008e0e8c941d7e01a12181931d5608aa366ee22228bd\"},\"account_number\":1,\"fee_rate\":1000}],\"id\":100}"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_adjust_account_transaction\",\"params\":[{\"item\":{\"type\":\"Identity\",\"value\":\"0x001bb92ee59d1320867801eaa949e5322545c416b8\"},\"from\":[{\"type\":\"Identity\",\"value\":\"0x00202647fecc5b9d8cbdb4ae7167e40f5ab1e4baaf\"}],\"asset_info\":{\"asset_type\":\"UDT\",\"udt_hash\":\"0xf21e7350fa9518ed3cbb008e0e8c941d7e01a12181931d5608aa366ee22228bd\"},\"account_number\":1,\"fee_rate\":1000}],\"id\":100}"
 wrk.headers["Content-Type"] = "application/json"
 
 function response(status, headers, body)
--- mercury-benchmark/script/testnet/build_deposit_transaction.lua
+++ mercury-benchmark/script/testnet/build_deposit_transaction.lua
@@ -1,5 +1,5 @@
 wrk.method = "POST"
-wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_dao_deposit_transaction\",\"params\":[{\"from\":{\"items\":[{\"type\":\"Address\",\"value\":\"ckt1qyqzqfj8lmx9h8vvhk62uut8us844v0yh2hsnqvvgc\"}],\"source\":\"Free\"},\"amount\":30000000000,\"fee_rate\":1000}],\"id\":100}"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_dao_deposit_transaction\",\"params\":[{\"from\":{\"items\":[{\"type\":\"Address\",\"value\":\"ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqfqyerlanzmnkxtmd9ww9n7gr66k8jt4tclm9jnk\"}],\"source\":\"Free\"},\"amount\":30000000000,\"fee_rate\":1000}],\"id\":100}"
 wrk.headers["Content-Type"] = "application/json"
 
 function response(status, headers, body)
