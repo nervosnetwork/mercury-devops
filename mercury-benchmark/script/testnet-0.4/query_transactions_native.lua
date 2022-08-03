b85d4d43668cc68877e14b5c4432bed44f7ee48b
EthanYuan <yuanchengxing@hotmail.com>
2022-08-03 23:59:45 +0800

    update scripts of query_transactions for 0.4

---
+++ mercury-benchmark/script/testnet-0.4/query_transactions_native.lua
@@ -0,0 +1,10 @@
+wrk.method = "POST"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"query_transactions\",\"params\":[{\"item\":{\"type\":\"Identity\",\"value\":\"0x001a4ff63598e43af9cd42324abb7657fa849c5bc3\"},\"asset_infos\":[{\"asset_type\":\"CKB\",\"udt_hash\":\"0x0000000000000000000000000000000000000000000000000000000000000000\"}],\"pagination\":{\"cursor\":null,\"order\":\"desc\",\"limit\":\"0x32\",\"return_count\":false},\"structure_type\":\"Native\"}],\"id\":100}"
+wrk.headers["Content-Type"] = "application/json"
+
+function response(status, headers, body)
+    if (string.find(body, '"error"')) then
+        print('error, resp: ', body)
+        wrk.thread:stop()
+    end
+end
