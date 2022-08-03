f9a4fb8ff9e979109b38149de472f5533e8efa48
EthanYuan <yuanchengxing@hotmail.com>
2022-08-03 18:52:38 +0800

    add v0.4 script of build_deposit_transaction

---
+++ mercury-benchmark/script/testnet-0.4/build_deposit_transaction.lua
@@ -0,0 +1,10 @@
+wrk.method = "POST"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_dao_deposit_transaction\",\"params\":[{\"from\":[{\"type\":\"Address\",\"value\":\"ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqfqyerlanzmnkxtmd9ww9n7gr66k8jt4tclm9jnk\"}],\"amount\":\"0x6FC23AC00\",\"fee_rate\":\"0x3E8\"}],\"id\":100}"
+wrk.headers["Content-Type"] = "application/json"
+
+function response(status, headers, body)
+    if (string.find(body, '"error"')) then
+        print('error, resp: ', body)
+        wrk.thread:stop()
+    end
+end
