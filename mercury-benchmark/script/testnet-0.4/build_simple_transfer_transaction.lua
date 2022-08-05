d714ba5f55c2d5103458fa1de62f80f099f7c3c0
EthanYuan <yuanchengxing@hotmail.com>
2022-08-05 21:57:01 +0800

    add build_simple_transfer_transaction test script for 0.4 benchmark.

---
+++ mercury-benchmark/script/testnet-0.4/build_simple_transfer_transaction.lua
@@ -0,0 +1,11 @@
+wrk.method = "POST"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_simple_transfer_transaction\",\"params\":[{\"asset_info\":{\"asset_type\":\"UDT\",\"udt_hash\":\"0xf21e7350fa9518ed3cbb008e0e8c941d7e01a12181931d5608aa366ee22228bd\"},\"from\":[\"ckt1qyq27z6pccncqlaamnh8ttapwn260egnt67ss2cwvz\"],\"to\":[{\"address\":\"ckt1qypg88ccqm59ksxp85788pnqg4rkejdgcg2qggxamt\",\"amount\":\"0x14\"}],\"fee_rate\":\"0x3e8\"}],\"id\":100}"
+wrk.headers["Content-Type"] = "application/json"
+
+
+function response(status, headers, body)
+    if (string.find(body, '"error"')) then
+        print('error, resp: ', body)
+        wrk.thread:stop()
+    end
+end
