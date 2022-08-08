5c9f4751e91aaa1e8fbccbede9031cef0a08fc20
EthanYuan <yuanchengxing@hotmail.com>
2022-08-08 15:27:43 +0800

    change para to lower case.

--- mercury-benchmark/script/testnet-0.4/build_deposit_transaction.lua
+++ mercury-benchmark/script/testnet-0.4/build_deposit_transaction.lua
@@ -1,5 +1,5 @@
 wrk.method = "POST"
-wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_dao_deposit_transaction\",\"params\":[{\"from\":[{\"type\":\"Address\",\"value\":\"ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqfqyerlanzmnkxtmd9ww9n7gr66k8jt4tclm9jnk\"}],\"amount\":\"0x6FC23AC00\",\"fee_rate\":\"0x3E8\"}],\"id\":100}"
+wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"build_dao_deposit_transaction\",\"params\":[{\"from\":[{\"type\":\"Address\",\"value\":\"ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqfqyerlanzmnkxtmd9ww9n7gr66k8jt4tclm9jnk\"}],\"amount\":\"0x6fc23ac00\",\"fee_rate\":\"0x3e8\"}],\"id\":100}"
 wrk.headers["Content-Type"] = "application/json"
 
 function response(status, headers, body)
